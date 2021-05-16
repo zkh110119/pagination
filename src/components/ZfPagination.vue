6ee1ab5f7be9da830649a24f6ef2dd605d48a5e6
zkh <493014828@qq.com>
2021-05-16 16:54:44 +0800

    修复问题    代码格式化

--- src/components/ZfPagination.vue
+++ src/components/ZfPagination.vue
@@ -1,35 +1,75 @@
 <template>
   <ul class="zf-ui-pagination">
     <li>
-      <el-select v-model="pageSize" :size="'mini'" class="zf-ui-pagination-page-size" @change="changePageSize">
-        <el-option v-for="size of pageSizes" :key="size" :label="size" :value="size"></el-option>
+      <el-select
+        class="zf-ui-pagination-page-size"
+        v-model="pageSize"
+        :size="'mini'"
+        @change="changePageSize">
+        <el-option
+          v-for="size of pageList"
+          :key="size"
+          :label="size"
+          :value="size"></el-option>
       </el-select>
     </li>
     <li>
-      <el-link :underline="false" icon="el-icon-d-arrow-left" @click="first"></el-link>
+      <el-link
+        icon="el-icon-d-arrow-left"
+        :underline="false"
+        @click="first"></el-link>
     </li>
     <li>
-      <el-link :underline="false" icon="el-icon-arrow-left" @click="prev"></el-link>
-    </li>
-    <li><span class="zf-ui-pagination-page-text-prev">第</span></li>
-    <li>
-      <el-input-number v-model="page" @blur="pageBlur" @keyup.enter.native="changePage" :min="1"
-                       :max="totalPage" :size="'mini'" :controls="false"
-                       class="zf-ui-pagination-page"></el-input-number>
+      <el-link
+        icon="el-icon-arrow-left"
+        :underline="false"
+        @click="prev"></el-link>
     </li>
     <li>
-      <span class="zf-ui-pagination-page-text-next">/{{totalPage}}页</span>
+      <span class="zf-ui-pagination-page-text-prev">
+        第
+      </span>
     </li>
     <li>
-      <el-link :underline="false" icon="el-icon-arrow-right" @click="next"></el-link>
+      <el-input-number
+        v-model="page"
+        @blur="pageBlur"
+        @keyup.enter.native="changePage"
+        :min="1"
+        :max="pages"
+        :size="'mini'"
+        :controls="false"
+        class="zf-ui-pagination-page"></el-input-number>
     </li>
     <li>
-      <el-link :underline="false" icon="el-icon-d-arrow-right" @click="last"></el-link>
+      <span class="zf-ui-pagination-page-text-next">
+        /{{pages}}页
+      </span>
     </li>
     <li>
-      <el-link :underline="false" icon="el-icon-refresh-right" @click="emitEvent"></el-link>
+      <el-link
+        icon="el-icon-arrow-right"
+        :underline="false"
+        @click="next">
+      </el-link>
     </li>
-    <li v-show="total > 0" style="float: right;">显示{{startNum}}到{{endNum}}条，共{{total}}条</li>
+    <li>
+      <el-link
+        icon="el-icon-d-arrow-right"
+        :underline="false"
+        @click="last"></el-link>
+    </li>
+    <li>
+      <el-link
+        icon="el-icon-refresh-right"
+        :underline="false"
+        @click="refresh"></el-link>
+    </li>
+    <li class="pull-right" v-show="total > 0">
+      <span>
+        显示{{from}}到{{to}}条，共{{total}}条
+      </span>
+    </li>
   </ul>
 </template>
 
@@ -37,6 +77,7 @@
 export default {
   name: 'ZfPagination',
   props: {
+    // 数据总条数
     total: {
       type: Number,
       required: true,
@@ -45,15 +86,21 @@ export default {
   },
   data () {
     return {
-      page: 1,
-      oldPage: 1,
-      pageSizes: [20, 50, 100, 200],
-      pageSize: 50
+      page: 1, // 页面数
+      oldPage: 1, // 记录页码数
+      pageList: [20, 50, 100, 200], // 每页展示多少条数据的集合
+      pageSize: 50 // 每页展示多少数据
     }
   },
   watch: {
-    totalPage (val, oldVal) {
+    /**
+       * 监听总页数数值变化
+       * @param val 新的pages
+       * @param oldVal 变化前的pages
+       */
+    pages (val, oldVal) {
       if (val && val !== oldVal) {
+        // 如果当前页面数大于总页码数，将当前页面数修改为总页码数
         if (this.page > val) {
           this.page = val
           this.oldPage = val
@@ -62,26 +109,47 @@ export default {
     }
   },
   methods: {
+    /**
+       * 跳转到首页，并触发分页数据变化事件
+       */
     first () {
       this.page = 1
       this.emitEvent()
     },
+    /**
+       * 跳转到上一页，并触发分页数据变化事件
+       */
     prev () {
       if (this.page > 1) {
         this.page--
         this.emitEvent()
       }
     },
+    /**
+       * 跳转到下一页，并触发分页数据变化事件
+       */
     next () {
-      if (this.page < this.totalPage) {
+      if (this.page < this.pages) {
         this.page++
         this.emitEvent()
       }
     },
+    /**
+       * 跳转到末尾页，并触发分页数据变化事件
+       */
     last () {
-      this.page = this.totalPage
+      this.page = this.pages
       this.emitEvent()
     },
+    /**
+       * 刷新事件
+       */
+    refresh () {
+      this.emitEvent()
+    },
+    /**
+       * 页码数输入框回车事件
+       */
     changePage () {
       if (this.page) {
         if (this.page !== this.oldPage) {
@@ -92,16 +160,26 @@ export default {
         this.page = this.oldPage
       }
     },
+    /**
+       * 每页展示条数变化时触发事件
+       */
     changePageSize () {
-      if (this.page > this.totalPage) {
-        this.page = this.totalPage
-        this.oldPage = this.totalPage
+      // 重新计算当前页码数是否大于总页码数，防止watch监听还没从触发就调用分页数据变化通知方法，导致page错误
+      if (this.page > this.pages) {
+        this.page = this.pages
+        this.oldPage = this.pages
       }
       this.emitEvent()
     },
+    /**
+       * 当前页码数输入框失去焦点时触发事件
+       */
     pageBlur () {
       this.page = this.oldPage
     },
+    /**
+       * 分页数据变化通知方法
+       */
     emitEvent () {
       this.$emit('pageOrSizeChange', {
         page: this.page,
@@ -110,13 +188,25 @@ export default {
     }
   },
   computed: {
-    totalPage () {
+    /**
+       * 计算总页码数
+       * @returns {number}
+       */
+    pages () {
       return this.total === 0 ? 1 : Math.ceil(this.total / this.pageSize)
     },
-    startNum () {
+    /**
+       * 计算当前页开始条数
+       * @returns {number}
+       */
+    from () {
       return this.total === 0 ? 0 : this.page ? ((this.page - 1) * this.pageSize + 1) : ((this.oldPage - 1) * this.pageSize + 1)
     },
-    endNum () {
+    /**
+       * 计算当前页结束条数
+       * @returns {number}
+       */
+    to () {
       return this.total === 0 ? 0 : this.page ? this.page * this.pageSize < this.total ? this.page * this.pageSize : this.total : this.oldPage * this.pageSize
     }
   }
@@ -138,6 +228,10 @@ export default {
       height: 100%;
       line-height: 28px;
       font-size: 14px;
+
+      &.pull-right {
+        float: right;
+      }
 
       .el-link {
         width: 28px;
